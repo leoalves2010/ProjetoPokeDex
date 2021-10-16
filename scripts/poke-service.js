e44b6fe5903af670a5f3e78be55f06cdd13c11e5
Leonardo Dvulatk <leoalves_2010@yahoo.com.br>
2021-10-16 18:35:31 -0300

    Inserção do Favicon e implementação do pokeServices com a lógica de requisição à API e modelagem dos dados

---
+++ scripts/poke-service.js
@@ -0,0 +1,34 @@
+export const pokeService = {
+    get url(){
+        return "//pokeapi.co/api/v2";
+    },
+    list: [],
+    listAll(){
+        if(this.list.length){
+            return Promise.resolve(this.list);
+        }else{
+            return fetch(`${this.url}/pokedex/1`)
+                .then(response => response.json())
+                .then(response => response.pokemon)
+                .then(pkmList => {
+                    pkmList.map(pokemon => {
+                        var number = getNumberFromURL(pokemon.resource_uri);
+                        return Object.assign({}, pokemon, {number});
+                    })
+                    .filter(pokemon => pokemon.number < 1000)
+                    .sort((a, b) => (a.number > b.number) ? 1 : -1)
+                    .map(pokemon => {
+                        var number = (`000${pokemon.number}`).slice(-3);
+                        return Object.assign({}, pokemon, {number});
+                    });
+                })
+                .then(list => {
+                    this.list = list;
+                    return this.list;
+                })
+        }
+    },
+    getNumberFromURL(url){
+        return parseInt(url.replace(/.*\/(\d+)\/$/, '$1'));
+    }
+}
